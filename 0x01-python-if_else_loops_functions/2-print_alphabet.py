a7ab1abe56bf6799f420512192ba5ef3948d448e
root <jghariokwu@gmail.com>
2023-06-06 13:36:39 -0700

    solo

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
 
-for letter in range(ord('a'), ord('z')+1):
-    print(chr(letter), end='')
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
+
 
 
